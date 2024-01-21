d65e16f05d46ecfb53ed643dedfe02aa3e31a32a
Arjunbalan <arjunbalan1994@gmail.com>
2024-01-21 21:57:20 +0530

    started

---
+++ Day-1/intro.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+// This is an alert (This is a single line comment)
+/* This is called multiline comment
+    alert("Hello World");
+    let a = 10;
+    let b = 20;
+    const sum = a + b;
+    alert(sum);
+*/
+// variables
+let firstName;
+firstName = "Arjun";
+let lastName = "Arasu";
+let fullName = firstName + " " + lastName;
+console.log("Hello, My name is" + " " + fullName);
