3bad9526be434f50ff5cb7dbfe15bb907f1f3ee6
Arjun Arasu <arjunbalan1994@gmail.com>
2024-02-14 10:36:36 +0530

    callback fin

--- callback.js
+++ callback.js
@@ -24,8 +24,17 @@ greet(greetConsole("Arjun"));
 greet(greetHeading("Arjun"));
 */
 // Foreach Method
-let array = ["Arjun", "Dharini", "Samyuktha"];
-let printVal = (val) => {
-  console.log(val.toUpperCase());
-};
-array.forEach(printVal);
+// let array = ["Arjun", "Dharini", "Samyuktha"];
+// let printVal = (val) => {
+//   console.log(val.toUpperCase());
+// };
+// array.forEach(printVal);
+let myArr = ["Orange", "Apple", "Grapes", "Pears", "Banana"];
+myArr.forEach((val) => {
+  // const opt = document.createElement("option");
+  // opt.innerHTML = val;
+  // opt.value = val;
+  let mySelect = (document.getElementById(
+    "fruits"
+  ).innerHTML += `<option>${val}</option>`);
+});
